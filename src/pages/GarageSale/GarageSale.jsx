74ce617557e965e184c3510b7eb8ab54fc7f9831
sartajhajam <hajamsaj@gmail.com>
2025-10-11 13:04:51 +0530

    Add RecentlyViewed component to Garage Sale page - resolve merge conflicts

--- src/pages/GarageSale/GarageSale.jsx
+++ src/pages/GarageSale/GarageSale.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import GarageSaleBanner from '../../components/GarageSaleBanner'
+import GarageSaleRecentlyViewed from '../../components/GarageSaleRecentlyViewed'
 
 const GarageSale = () => {
     return (
-        <div className='w-full h-screen overflow-hidden'>
+        <div className='w-full min-h-screen'>
             <GarageSaleBanner />
+            {/* Recently Viewed Section */}
+            <GarageSaleRecentlyViewed />
         </div>
     )
 }
