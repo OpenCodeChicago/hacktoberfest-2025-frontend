1e69ff96a22bfea6ddd97e1e004383cf706c38c6
Alex Smagin <alexsmagin1@gmail.com>
2025-10-31 17:51:28 -0500

    fix: update dependencies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,8 @@
 import router from '../routes/router';
 import { RouterProvider } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
-import {useAuthInitializer} from '../hooks/useAuthInitializer';
 
 export default function App() {
-  useAuthInitializer()
   return (
     <HelmetProvider>
       <RouterProvider router={router} />
--- src/hooks/useAuthInitializer.js
+++ src/hooks/useAuthInitializer.js
@@ -1,16 +1,16 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { loginSuccess } from "../store/authSlice";
+// import { useEffect } from "react";
+// import { useDispatch } from "react-redux";
+// import { loginSuccess } from "../store/authSlice";
 
-export function useAuthInitializer() {
-    const dispatch = useDispatch();
+// export function useAuthInitializer() {
+//     const dispatch = useDispatch();
 
-    useEffect(() => {
-        const token = localStorage.getItem("token");
-        const user = localStorage.getItem("user");
+//     useEffect(() => {
+//         const token = localStorage.getItem("token");
+//         const user = localStorage.getItem("user");
 
-        if (token && user) {
-        dispatch(loginSuccess({ token, user: JSON.parse(user) }));
-        }
-    }, [dispatch]);
-}
+//         if (token && user) {
+//         dispatch(loginSuccess({ token, user: JSON.parse(user) }));
+//         }
+//     }, [dispatch]);
+// }
